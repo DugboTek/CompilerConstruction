32bac4044b06fda98da5ba93d3c00db60c6c3513
Sola Dugbo <sola.dugbo@publicissapient.com>
2024-02-06 13:50:31 -0500

    updated lab 2

---
+++ Lab2/odugbo_math_scanner/src/ParserTokens.java
@@ -0,0 +1,12 @@
+/**
+ * Parser token constants
+ * Name: Dr. Tom Way
+ */
+public interface ParserTokens
+{
+	static public final int ENDINPUT = -1;
+	static public final int ADDOP = 1;
+	static public final int NUMBER = 20;
+	static public final int NEWLINE = 90;
+	static public final int error = 99;
+}
